885a32b58a3e22f179328dcce2e090b22f7ae704
Daniel Cardenas <dan@Daniels-MacBook-Pro.local>
2021-02-22 20:34:34 -0500

    layout

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 //External
 import Head from 'next/head';
-import { Button, Heading, Text, Code } from '@chakra-ui/react';
+import { Button, Heading, Text, Code, Flex } from '@chakra-ui/react';
 import { Icon } from '@chakra-ui/icons';
 
 //Internal
@@ -9,35 +9,33 @@ import { useAuth } from '@/lib/auth';
 const IconLogo = (props) => (
   <Icon viewBox="0 0 46 32" {...props}>
     <path
-          d="M19.557.113C11.34.32 9.117 8.757 9.03 12.95c1.643-2.67 4.62-3.08 6.931-3.08 2.825.085 10.27.205 17.458 0C40.61 9.663 44.802 3.28 46 .112c-5.391-.085-18.228-.205-26.443 0zM14.422 14.234C3.332 14.234-.468 24.76.045 31.948c3.594-6.418 7.617-7.53 9.243-7.445h6.675c5.956 0 11.039-6.846 12.836-10.27H14.422z"
-          fill="currentColor"
-        />
+      d="M19.557.113C11.34.32 9.117 8.757 9.03 12.95c1.643-2.67 4.62-3.08 6.931-3.08 2.825.085 10.27.205 17.458 0C40.61 9.663 44.802 3.28 46 .112c-5.391-.085-18.228-.205-26.443 0zM14.422 14.234C3.332 14.234-.468 24.76.045 31.948c3.594-6.418 7.617-7.53 9.243-7.445h6.675c5.956 0 11.039-6.846 12.836-10.27H14.422z"
+      fill="currentColor"
+    />
   </Icon>
-)
+);
 
 const Home = () => {
   const auth = useAuth();
 
   return (
-    <div className="container">
+    <Flex as="main" direction="column" align="center" justify="center" height="100vh">
       <Head>
         <title>Feedbacknow</title>
       </Head>
-      <main>
-        <Heading fontWeight="bold">Feedbacknow</Heading>
-        <Text>
-          Current user: <Code>{auth.user ? auth.user.email : 'None'}</Code>
-        </Text>
-        <IconLogo />
-        {auth.user ? (
-          <Button bg="brand.100" onClick={(e) => auth.signout()}>
-            Sign Out
-          </Button>
-        ) : (
-          <Button onClick={(e) => auth.signinWithGitHub()}>Sign In</Button>
-        )}
-      </main>
-    </div>
+      <Heading fontWeight="bold">Feedbacknow</Heading>
+      <Text>
+        Current user: <Code>{auth.user ? auth.user.email : 'None'}</Code>
+      </Text>
+      <IconLogo width="128px" height="128px" />
+      {auth.user ? (
+        <Button bg="brand.100" onClick={(e) => auth.signout()}>
+          Sign Out
+        </Button>
+      ) : (
+        <Button onClick={(e) => auth.signinWithGitHub()}>Sign In</Button>
+      )}
+    </Flex>
   );
 };
 
